5d97e0287cd685b3f8d6f4f198d98f0af7ec26b9
veersudhir83 <veersudhir83@gmail.com>
2017-09-13 10:26:42 +0530

    Increased history tracking to 20 builds and artifacts

--- src/DevOps-Pipeline.groovy
+++ src/DevOps-Pipeline.groovy
@@ -6,7 +6,7 @@
 
 /* Only keep the 10 most recent builds. */
 def projectProperties = [
-        buildDiscarder(logRotator(artifactDaysToKeepStr: '5', artifactNumToKeepStr: '5', daysToKeepStr: '5', numToKeepStr: '5')),
+        buildDiscarder(logRotator(artifactDaysToKeepStr: '20', artifactNumToKeepStr: '20', daysToKeepStr: '20', numToKeepStr: '20')),
         [$class: 'GithubProjectProperty', projectUrlStr: 'https://github.com/veersudhir83/DevOps-Pipeline.git/']
         //,pipelineTriggers([pollSCM('H/10 * * * *')])
 ]
