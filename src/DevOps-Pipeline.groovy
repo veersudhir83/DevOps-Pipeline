bfdbf2a341d9c3a8be9ddc7a66522c1472d0bc5c
veersudhir83 <veersudhir83@gmail.com>
2017-09-13 09:26:57 +0530

    Updated Publish HTML step

--- src/DevOps-Pipeline.groovy
+++ src/DevOps-Pipeline.groovy
@@ -269,6 +269,7 @@ try {
                 publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'devops-web-maven/target/site/apidocs', reportFiles: 'index.html', reportName: 'HTML Report', reportTitles: ''])
             } else {
                 junit '**\\devops-web-maven\\target\\surefire-reports\\*.xml'
+                publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'devops-web-maven\\target\\site\\apidocs', reportFiles: 'index.html', reportName: 'HTML Report', reportTitles: ''])
             }
         }
 
